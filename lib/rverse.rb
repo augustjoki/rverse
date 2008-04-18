37c5b9bb910ac1bb9ba5c1ee00df13a4b2de25bc
August Joki <august@Monkey-Head.local>
2008-04-17 17:44:53 -0700

    redirect stdout if second arg

--- lib/rverse.rb
+++ lib/rverse.rb
@@ -1,8 +1,14 @@
 #! /usr/bin/env ruby
 require 'objc'
 require 'mach-o'
+
+orig_stdout = $stdout
+
 if ARGV.length
   file = ARGV[0]
+  if ARGV.length == 2
+    $stdout = File.new(ARGV[1], 'w')
+  end
 else
   file = "/Volumes/Vail5A225c.CarrierBundle/Applications/MobilePhone.app/MobilePhone"
 end
@@ -14,3 +20,5 @@ macho = MachO.new(f).architectures[[:arm, :v6]]
 objc = ObjectiveC.new(f, macho)
 
 f.close
+
+$stdout = orig_stdout
